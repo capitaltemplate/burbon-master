06c29771f4556aeb751985ecdaaa902d14749f4c
waksiu <gentoo2x@gmail.com>
2018-02-22 21:29:50 +0100

    jumbotron

--- js/javascript.js
+++ js/javascript.js
@@ -131,12 +131,6 @@ $(document).ready(function( $ ) {
         type: 'char'
     });
 
-
-    // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow':'visible'});
-
     // counterUp
     $('span.counter').counterUp({
        delay: 10, // the delay time in ms
@@ -183,5 +177,10 @@ $(document).ready(function( $ ) {
             }
         });
     }
+
+    // preloader
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({'overflow':'visible'});
 });
 
